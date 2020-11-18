02c7c254ab84ccbb24075b0e0c941355fa2d42ee
Dmytro Bubon <dmitry.bubon@gmail.com>
2020-11-18 15:37:09 +0200

    Adds stickyness and responsiveness to NavBar

--- components/core/NavBar/index.js
+++ components/core/NavBar/index.js
@@ -1,14 +1,21 @@
 import React, { useMemo } from "react"
-import { Box, Flex, Stack } from "@rent_avail/layout"
+import { Box, Container, Flex } from "@rent_avail/layout"
 import { Button } from "@rent_avail/controls"
-import styled from "styled-components"
+import styled, { useTheme } from "styled-components"
 
 const NavBarButton = styled(Button)`
   border: none;
   height: 48px;
 `
 
-const NavBar = ({ background = "ui_100", links = [] }) => {
+const NavBar = ({
+  background = "ui_100",
+  links = [],
+  containerWidth = "96rem",
+  sticky,
+  ...props
+}) => {
+  const { breakpoints } = useTheme()
   const [linksSorted, splitIndex] = useMemo(() => {
     const sorted = links.sort(({ push: a }, { push: b }) => {
       if (a === b) {
@@ -19,30 +26,52 @@ const NavBar = ({ background = "ui_100", links = [] }) => {
     return [sorted, sorted.findIndex(({ push }) => push)]
   }, [links])
   return (
-    <Flex p="2rem" bg={background}>
-      <Box as="a" href="https://avail.co" mr="2rem">
-        <img
-          src="/icons/icon-100.png"
-          aria-label="Avail"
-          alt="Avail"
-          height={46}
-          width={46}
-        />
-      </Box>
-      <Stack direction={["row"]} spacing="2rem" flex="1 1 auto">
-        {linksSorted.map((link, index) => (
-          <NavBarButton
-            variant={link.primary ? "primary" : "default"}
-            key={link.href}
-            href={link.href}
-            ml={index === splitIndex ? "auto" : "0"}
-            forwardedAs="a"
-          >
-            {link.text}
-          </NavBarButton>
-        ))}
-      </Stack>
-    </Flex>
+    <Box
+      bg={background}
+      position={sticky ? "sticky" : "static"}
+      top="0"
+      zIndex="1"
+      {...props}
+    >
+      <Container p={["1rem", "2rem"]} maxWidth={containerWidth}>
+        <Flex>
+          <Box as="a" href="https://avail.co" mr="2rem">
+            <img
+              src="/icons/icon-100.png"
+              aria-label="Avail"
+              alt="Avail"
+              height={48}
+              width={48}
+            />
+          </Box>
+          <Flex flexDirection="row" width="100%">
+            {linksSorted.map((link, index) => {
+              const bpIndex = breakpoints.indexOf(link.breakpoint)
+              const display =
+                bpIndex !== -1
+                  ? Array(bpIndex + 2)
+                      .fill("none")
+                      .fill("block", bpIndex + 1)
+                  : "block"
+              return (
+                <React.Fragment key={link.href}>
+                  {index === splitIndex && <Box ml="auto" key="separator" />}
+                  <NavBarButton
+                    variant={link.primary ? "primary" : "default"}
+                    href={link.href}
+                    forwardedAs="a"
+                    display={display}
+                    mr={index === links.length - 1 ? "0" : "2rem"}
+                  >
+                    {link.text}
+                  </NavBarButton>
+                </React.Fragment>
+              )
+            })}
+          </Flex>
+        </Flex>
+      </Container>
+    </Box>
   )
 }
 
