59092218982cfe8b444efc510793afd3095311e1
Dmytro Bubon <dmitry.bubon@gmail.com>
2020-11-19 20:21:25 +0200

    Adds contrast buttons to Hero

--- slices/HeroUnit/index.js
+++ slices/HeroUnit/index.js
@@ -4,8 +4,49 @@ import { Button } from "@rent_avail/controls"
 import RichText from "prismic-reactjs/src/Component"
 import Heading from "components/prismic/Heading"
 import { Hero } from "components/core/Hero"
-import { useTheme } from "styled-components"
+import styled, { useTheme } from "styled-components"
 import { availContainerWidth } from "lib/config"
+
+const isDark = (scheme, intensity) => {
+  switch (scheme) {
+    case "straw":
+    case "gold":
+      return intensity > 700
+    case "ui":
+      return intensity > 500
+    default:
+      return intensity > 300
+  }
+}
+
+const ContrastButton = styled(Button)(({ theme: { colors }, scheme }) => ({
+  background: "transparent",
+  borderColor: colors[`${scheme}_100`],
+  color: colors[`${scheme}_100`],
+  "&:hover": {
+    background: colors[`${scheme}_100`],
+    color: colors[`${scheme}_500`],
+  },
+  "&:focus": {
+    background: colors[`${scheme}_100`],
+  },
+}))
+
+const ContrastPrimaryButton = styled(Button)(
+  ({ theme: { colors }, scheme }) => ({
+    background: colors[`${scheme}_100`],
+    borderColor: colors[`${scheme}_100`],
+    color: colors[`${scheme}_500`],
+    "&:hover": {
+      background: colors.ui_100,
+      borderColor: colors.ui_100,
+      color: colors[`${scheme}_700`],
+    },
+    "&:focus": {
+      background: colors.ui_100,
+    },
+  })
+)
 
 const HeroUnit = ({ slice }) => {
   const {
@@ -24,22 +65,13 @@ const HeroUnit = ({ slice }) => {
     },
   } = slice
 
-  const { colors } = useTheme()
-
-  const darkBg = useMemo(() => {
+  const [scheme, dark] = useMemo(() => {
     if (typeof background !== "string") {
       return false
     }
+    // eslint-disable-next-line no-shadow
     const [scheme, intensity] = background.split("_")
-    switch (scheme) {
-      case "straw":
-      case "gold":
-        return intensity > 700
-      case "ui":
-        return intensity > 500
-      default:
-        return intensity > 300
-    }
+    return [scheme, isDark(scheme, intensity)]
   }, [background])
 
   return (
@@ -54,28 +86,28 @@ const HeroUnit = ({ slice }) => {
       primaryLink={
         primaryButtonText &&
         primaryButtonLink && (
-          <Link href={primaryButtonLink.url}>
-            <Button
-              {...(darkBg
-                ? { color: "gold_500", textColor: colors[background] }
-                : { variant: "primary" })}
-            >
-              {primaryButtonText}
-            </Button>
+          <Link href={primaryButtonLink.url} passHref>
+            {dark ? (
+              <ContrastPrimaryButton scheme={scheme}>
+                {primaryButtonText}
+              </ContrastPrimaryButton>
+            ) : (
+              <Button variant="primary">{primaryButtonText}</Button>
+            )}
           </Link>
         )
       }
       secondaryLink={
         secondaryButtonText &&
         secondaryButtonLink && (
-          <Link href={secondaryButtonLink.url}>
-            <Button
-              {...(darkBg
-                ? { color: "gold_500", textColor: colors[background] }
-                : null)}
-            >
-              {secondaryButtonText}
-            </Button>
+          <Link href={secondaryButtonLink.url} passHref>
+            {dark ? (
+              <ContrastButton scheme={scheme}>
+                {secondaryButtonText}
+              </ContrastButton>
+            ) : (
+              <Button>{secondaryButtonText}</Button>
+            )}
           </Link>
         )
       }
