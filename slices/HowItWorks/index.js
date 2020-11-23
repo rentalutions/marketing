7c232f2e91cb944a812e21ce8b5f4776bc9a78be
Dmytro Bubon <dmitry.bubon@gmail.com>
2020-11-23 12:29:55 +0200

    Add anchor to HowItWorks slice

--- slices/HowItWorks/index.js
+++ slices/HowItWorks/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import RichText from "components/prismic/RichText"
 import { HowItWorks } from "components/core/HowItWorks"
 import { availContainerWidth } from "lib/config"
+import Anchor from "components/prismic/Anchor"
 
 const MySlice = ({ slice }) => {
   const {
-    primary: { title, background, flip },
+    primary: { title, background, flip, hash },
   } = slice
   // eslint-disable-next-line no-shadow
   const sections = slice.items.map(({ title, description, image }, idx) => ({
@@ -19,15 +20,18 @@ const MySlice = ({ slice }) => {
     image,
   }))
   return (
-    <HowItWorks
-      title={<RichText render={title} heading />}
-      sections={sections}
-      bg={background}
-      containerWidth={availContainerWidth}
-      py="2rem"
-      alternate={flip ? (idx) => idx % 2 === 0 : null}
-      color={background === "blue_500" ? "blue_100" : "inherit"}
-    />
+    <React.Fragment>
+      {hash && <Anchor hash={hash} />}
+      <HowItWorks
+        title={<RichText render={title} heading />}
+        sections={sections}
+        bg={background}
+        containerWidth={availContainerWidth}
+        py="2rem"
+        alternate={flip ? (idx) => idx % 2 === 0 : undefined}
+        color={background === "blue_500" ? "blue_100" : "inherit"}
+      />
+    </React.Fragment>
   )
 }
 
