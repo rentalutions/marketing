fb995487b45c23a21bdf15cffc92436746eaa4fc
Dmytro Bubon <dmitry.bubon@gmail.com>
2020-12-07 20:11:02 +0200

    Add segment code snippet

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,7 @@ export default class Document extends NextDoc {
 
   render() {
     const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID
+    const SEGMENT_KEY = process.env.NEXT_PUBLIC_SEGMENT_KEY
     return (
       <Html>
         <Head>
@@ -43,7 +44,7 @@ export default class Document extends NextDoc {
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
            })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`.replace(
-                  /(\r\n|\n|\r)/gm,
+                  /(\r\n|\n|\r|\t)/gm,
                   ""
                 ),
               }}
@@ -56,7 +57,27 @@ export default class Document extends NextDoc {
           h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
           (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
           })(window,document.documentElement,'async-hide','dataLayer',4000,
-          {'${GTM_CONTAINER_ID}':true});`.replace(/(\r\n|\n|\r)/gm, ""),
+          {'${GTM_CONTAINER_ID}':true});`.replace(/(\r\n|\n|\r|\t)/gm, ""),
+              }}
+            />
+          )}
+          {SEGMENT_KEY && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)
+          if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");
+          else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview",
+          "identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware",
+          "addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=
+          function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);
+          return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=
+          analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");
+          n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";
+          var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};
+          analytics.SNIPPET_VERSION="4.1.0";analytics.load("${SEGMENT_KEY}");analytics.page();}}();`.replace(
+                  /(\r\n|\n|\r|\t)/gm,
+                  ""
+                ),
               }}
             />
           )}
