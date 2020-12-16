a62ff4fb98901427ffeb08b68b7eeb0ea4f121fc
Patrick Krawczykowski <patrick@dreadful.design>
2020-12-16 12:16:08 -0600

    changed to fit code standards

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
-import { Router } from "next/router"
 import { prismicClient } from "src/prismic.config"
 
-const IndexPage = () => {
+export default function IndexPage() {
   return null
 }
 
-/** TODO: Come up with a tag to indicate "production" pages in CMS and
- *        redirect to the first "production" page instead */
-const possibleLandingPages = ["listings-100001", "listings"]
+/**
+ * TODO: Indicate production pages with a tag.
+ * Redirect to first production page instead of hard coding it with an array.
+ */
 
-IndexPage.getInitialProps = async ({ res }) => {
-  const { results: pages } = await prismicClient.query("")
+// const landingPages = ["listings"]
 
-  const landingPage =
-    pages.find((page) => possibleLandingPages.includes(page.uid)) || pages[0]
-
-  if (res) {
-    res.writeHead(302, { Location: `/${landingPage.type}/${landingPage.uid}` })
-    res.end()
-    return {}
+export async function getServerSideProps() {
+  const {
+    results: [page],
+  } = await prismicClient.query("")
+  if (page) {
+    return {
+      redirect: {
+        destination: `/${page.type}/${page.uid}`,
+        permanent: false,
+      },
+    }
   }
-
-  Router.push(`/info/${landingPage.uid}`)
-  return {}
+  return {
+    redirect: {
+      destination: "/info/listings",
+      permanent: false,
+    },
+  }
 }
-
-export default IndexPage
