bdd3444b1765a939484d3191643a5fa5a7a0372e
Henrique Campos de Freitas <henriquecfreitas@hotmail.com.br>
2021-02-19 15:08:06 -0600

    Refactoring BoxedTitleSection

--- src/components/molecules/BoxedTitleSection/index.js
+++ src/components/molecules/BoxedTitleSection/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 import { Box, Container, Flex } from "@rent_avail/layout"
-import { useResize } from "@rent_avail/utils"
 
 function BoxedTitleSection({
   containerWidth,
@@ -12,72 +11,63 @@ function BoxedTitleSection({
   children,
   ...props
 }) {
-  const containerRef = useRef()
-  const containerRect = useResize(containerRef)
-
-  const [titleSpace, setTitleSpace] = useState("0px")
   const titleBoxVariants = {
     right: {
       textAlign: "right",
-      padding: `4rem 2rem 4rem ${titleSpace}`,
-      ml: `-${titleSpace}`,
       borderRadius: "0 2.5rem 2.5rem 0",
     },
     left: {
       textAlign: "left",
-      padding: `4rem ${titleSpace} 4rem 2rem`,
-      mr: `-${titleSpace}`,
       borderRadius: "2.5rem 0 0 2.5rem",
     },
     noOverflow: {
       textAlign: "center",
-      padding: "4rem 2rem",
       borderRadius: "2.5rem",
     },
   }
 
-  useEffect(() => {
-    const container = containerRef.current
-    if (!container || !getComputedStyle) return
-    const { marginLeft, paddingLeft } = getComputedStyle(container)
-    const titleSpaceInPixels = overflow
-      ? parseInt(marginLeft, 10) + parseInt(paddingLeft, 10)
-      : 0
-    setTitleSpace(`${titleSpaceInPixels}px`)
-  }, [containerRect])
-
   return (
-    <Container
-      ref={containerRef}
-      maxWidth={containerWidth}
-      bg={background}
-      {...props}
-    >
-      <Flex
-        my={2}
-        sx={{
-          gap: 1,
-          flexFlow: "row wrap",
-        }}
+    <Flex my={2}>
+      <Box
+        flex={1}
+        order={orientation === "right" ? 0 : 2}
+        bg={titleBackground}
+      />
+      <Container
+        flex="auto"
+        order={1}
+        maxWidth={containerWidth}
+        bg={background}
+        {...props}
+        px={0}
       >
         <Flex
-          flex={1}
-          bg={titleBackground}
-          alignItems="center"
-          borderRadius="2.5rem"
-          {...titleBoxVariants[overflow ? orientation : "noOverflow"]}
+          sx={{
+            gap: 1,
+            flexFlow: "row wrap",
+          }}
         >
-          <Box width="fill-available">{title}</Box>
+          <Flex
+            flex={1}
+            bg={titleBackground}
+            alignItems="center"
+            borderRadius="2.5rem"
+            padding="4rem 2rem"
+            {...titleBoxVariants[overflow ? orientation : "noOverflow"]}
+          >
+            <Box width="fill-available">{title}</Box>
+          </Flex>
+          <Box
+            width={["100%", "100%", "fit-content"]}
+            flex={["auto", "auto", 1]}
+            order={orientation === "left" ? ["unset", "unset", -1] : "unset"}
+          >
+            {children}
+          </Box>
         </Flex>
-        <Box
-          width={["100%", "100%", "fit-content"]}
-          flex={["auto", "auto", 1]}
-          order={orientation === "left" ? ["unset", "unset", -1] : "unset"}
-        >
-          {children}
-        </Box>
-      </Flex>
-    </Container>
+      </Container>
+      <Box flex={1} order={orientation === "right" ? 2 : 0} />
+    </Flex>
   )
 }
 
