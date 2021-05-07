b4240f24c1104a445fd436b0aae24fc26600297a
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-05-07 17:18:02 +0300

    Refactor tenants features page with static components

--- pages/tenants/features/index.page.js
+++ pages/tenants/features/index.page.js
@@ -1,86 +1,29 @@
 import React from "react"
-import { NextSeo } from "next-seo"
-import { useTheme } from "styled-components"
 
 import { Box } from "@rent_avail/layout"
 
-import NavBar from "components/organisms/NavBar"
-import AvailFooter from "components/partials/AvailFooter"
-
-import { CONTAINER_WIDTHS } from "config"
-
+import StaticBasePage from "components/static/StaticBasePage"
 import TenantFeaturesHero from "./components/tenants-features-hero"
 import TenantFeaturesCards from "./components/tenants-features-cards"
 import TenantFeaturesHowItWorks from "./components/tenants-features-how-it-works"
 import TenantFeaturesTestimonials from "./components/tenants-features-testimonials"
 import TenantFeaturesEmailCapture from "./components/tenants-features-email-capture"
 
-const primaryButton = {
-  text: "Sign up",
-  link: {
-    url: "https://www.avail.co/users/new",
-    target: "_blank",
-  },
-}
-
-const secondaryButton = {
-  text: "Log In",
-  link: {
-    url: "https://www.avail.co/login",
-    target: "_blank",
-  },
-}
-
-const links = [
-  {
-    text: "Pricing",
-    href: "/pricing",
-    target: "_blank",
-  },
-  {
-    text: "For Landlords",
-    href: "/landlords/features",
-    target: "_blank",
-  },
-]
-
 const TenantFeatures = () => {
-  const title = "Tenant Features | Avail"
-  const description = ""
-  const url = "http://info.avail.co/tenants/features"
-
-  const { colors } = useTheme()
-
   return (
-    <React.Fragment>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          title,
-          description,
-          url,
-        }}
-      />
-      <NavBar
-        borderBottom={`1px solid ${colors.ui_500}`}
-        containerWidth={CONTAINER_WIDTHS}
-        type="Avail"
-        sticky
-        primaryButton={primaryButton}
-        secondaryButton={secondaryButton}
-        links={links}
-      />
+    <StaticBasePage
+      title="Tenant Features | Avail"
+      description=""
+      url="https://www.avail.co/tenants/features"
+    >
       <Box bg="ui_300" pb="18rem">
-        <TenantFeaturesHero primaryButtonLink={primaryButton?.link} />
+        <TenantFeaturesHero />
         <TenantFeaturesCards mt={3} />
         <TenantFeaturesHowItWorks mt={3} py={6} />
         <TenantFeaturesTestimonials mt={6} />
         <TenantFeaturesEmailCapture mt={4} />
       </Box>
-      <AvailFooter />
-    </React.Fragment>
+    </StaticBasePage>
   )
 }
 
