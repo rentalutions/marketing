c0590490a5ba9ddcb619c39a1b2e0fb707c8dbf6
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-05-19 14:27:37 +0300

    Refactor homepage

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,91 +1,26 @@
 import React from "react"
-import { NextSeo } from "next-seo"
-import { useTheme } from "styled-components"
 
-import { Box } from "@rent_avail/layout"
-
-import NavBar from "components/organisms/NavBar"
-import AvailFooter from "components/partials/AvailFooter"
-
-import { CONTAINER_WIDTHS } from "config"
-
+import StaticBasePage from "components/static/StaticBasePage"
 import HomeHero from "./components/home-hero"
 import HomeHowItWorks from "./components/home-how-it-works"
 import HomeTestimonials from "./components/home-testimonials"
 import HomeShowcase from "./components/home-showcase"
 import HomeEmailCapture from "./components/home-email-capture"
 
-const primaryButton = {
-  text: "Try Now",
-  link: {
-    url: "https://www.avail.co/users/new",
-    target: "_blank",
-  },
-}
-
-const secondaryButton = {
-  text: "Log In",
-  link: {
-    url: "https://www.avail.co/login",
-    target: "_blank",
-  },
-}
-
-const links = [
-  {
-    text: "Pricing",
-    href: "/pricing",
-    target: "_blank",
-  },
-  {
-    text: "Features",
-    href: "/tenants/features",
-    target: "_blank",
-  },
-  {
-    text: "Use Cases",
-    href: "#use-cases",
-    target: "_self",
-  },
-]
-
 const TenantFeatures = () => {
-  const title = "Tenant Features | Avail"
-  const description = ""
-  const url = "http://info.avail.co/tenants/features"
-
-  const { colors } = useTheme()
-
   return (
-    <React.Fragment>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          title,
-          description,
-          url,
-        }}
-      />
-      <NavBar
-        borderBottom={`1px solid ${colors.ui_500}`}
-        containerWidth={CONTAINER_WIDTHS}
-        type="Avail"
-        sticky
-        primaryButton={primaryButton}
-        secondaryButton={secondaryButton}
-        links={links}
-      />
-      <Box bg="ui_300" pb="18rem">
-        <HomeHero mb="20rem" />
-        <HomeHowItWorks id="use-cases" />
-        <HomeTestimonials />
-        <HomeShowcase mt={8} />
-        <HomeEmailCapture mt={8} />
-      </Box>
-      <AvailFooter />
-    </React.Fragment>
+    <StaticBasePage
+      title="Free Landlord Software: Rental Listings, Tenant Screening &amp; More | Avail"
+      description="Free landlord software to help you manage your rental properties. List your rental, find and screen tenants, draft leases, collect rent online, and more."
+      url="https://info.avail.co/"
+      background="ui_300"
+    >
+      <HomeHero mb="20rem" />
+      <HomeHowItWorks id="use-cases" />
+      <HomeTestimonials />
+      <HomeShowcase my={6} />
+      <HomeEmailCapture my={6} />
+    </StaticBasePage>
   )
 }
 
