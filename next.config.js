d6db9ea3d4cafafd4c02fbe1e23a44cdcea8aad1
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-05-21 14:37:30 +0300

    Update next/image domains list

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   images: {
-    domains: ["images.prismic.io"],
+    domains: [
+      "images.prismic.io",
+      "avail-marketing.cdn.prismic.io",
+      "avail-marketing-staging.cdn.prismic.io",
+    ],
     deviceSizes: [480, 720, 960, 1080, 1200, 1440],
   },
   future: {
