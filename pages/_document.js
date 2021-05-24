117d6502340119742e125af339c99393dbe78f37
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-05-24 19:12:19 +0300

    Remove the GTM anti-flicker snippet

--- pages/_document.js
+++ pages/_document.js
@@ -50,17 +50,6 @@ export default class Document extends NextDoc {
               }}
             />
           )}
-          {GTM_CONTAINER_ID && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-          h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-          (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-          })(window,document.documentElement,'async-hide','dataLayer',4000,
-          {'${GTM_CONTAINER_ID}':true});`.replace(/(\r\n|\n|\r|\t)/gm, ""),
-              }}
-            />
-          )}
           {SEGMENT_KEY && (
             <script
               dangerouslySetInnerHTML={{
