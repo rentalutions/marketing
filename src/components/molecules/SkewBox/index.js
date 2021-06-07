9590246bb413ff45c6000f360c46ef4f4eb0f527
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-06-07 15:59:49 +0300

    Refactor SkewBox + fix layout order issue

--- src/components/molecules/SkewBox/index.js
+++ src/components/molecules/SkewBox/index.js
@@ -1,31 +1,34 @@
 import React from "react"
-import styled from "styled-components"
 import { Box } from "@rent_avail/layout"
 
-const Wrapper = styled(Box)`
-  position: relative;
-  .skew {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: ${({ theme, skewBg }) =>
-      skewBg ? theme.colors[skewBg] || skewBg : theme.colors.ui_100};
-    transform: skewY(${({ skew }) => (skew === "right" ? 4 : -4)}deg);
-    transform-origin: top ${({ skew }) => skew};
-  }
-`
-function SkewBox({ bg, skew = "none", children, ...props }) {
+function SkewBox({ bg, skew = "none", children, sx, ...props }) {
   return skew !== "none" ? (
-    <Wrapper {...props} skewBg={bg} skew={skew} pt="4rem" pb="10rem">
-      <div className="skew" />
-      {children}
-    </Wrapper>
+    <Box
+      {...props}
+      sx={{
+        position: "relative",
+        pb: "calc(100vw * 0.06)", // 0.06 is a static approximation based on 4deg skew angle
+        ...sx,
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          bg: bg || "transparent",
+          transform: `skewY(${skew === "right" ? 4 : -4}deg)`,
+          transformOrigin: `top ${skew}`,
+        }}
+      />
+      <Box sx={{ position: "relative" }}>{children}</Box>
+    </Box>
   ) : (
-    <Box {...props} bg={bg}>
+    <Box {...props} sx={{ bg, ...sx }}>
       {children}
     </Box>
   )
