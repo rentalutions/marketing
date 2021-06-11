79eab6203c12175a23bd9b8019ae2180ea6d90fe
Dmytro Bubon <dmitry.bubon@gmail.com>
2021-06-11 15:07:30 +0300

    Try disabling animations

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,8 @@ import { DefaultSeo } from "next-seo"
 import { BREAKPOINTS, DEFAULT_SEO } from "config"
 import { UIDReset } from "react-uid"
 import { AnalyticsProvider } from "utils/analytics/context"
-import { LazyMotion } from "framer-motion"
 import dynamic from "next/dynamic"
 
-const motionFeatures = () =>
-  import("utils/animation/features").then((res) => res.default)
 const PreviewWarning = dynamic(() =>
   import("components/partials/PreviewWarning")
 )
@@ -28,9 +25,7 @@ export default function App({ Component, pageProps }) {
         <Reset />
         {preview && <PreviewWarning />}
         <UIDReset prefix="uid_">
-          <LazyMotion features={motionFeatures}>
-            <Component {...pageProps} />
-          </LazyMotion>
+          <Component {...pageProps} />
         </UIDReset>
       </AnalyticsProvider>
     </ThemeProvider>
--- src/utils/animation/index.js
+++ src/utils/animation/index.js
@@ -71,10 +71,10 @@ export function useAnimation({
       container: null,
       item: null,
     },
-    fadeIn: makePreset({
-      initial: { opacity: 0, y: "1rem" },
-      final: { opacity: 1, y: "0rem" },
-    }),
+    fadeIn: {
+      container: null,
+      item: null,
+    },
     fadeOut: makePreset({
       initial: { opacity: 1, y: "0rem" },
       final: { opacity: 0, y: "-1rem" },
