2cdcc401f33256209dbcf9f3132fdf5d3578b930
Taylor Savegnago <taylor@avail.co>
2021-07-02 17:27:34 -0300

    Created tenants page

---
+++ pages/tenants/features/data/email-capture.js
@@ -0,0 +1,20 @@
+export default {
+    slice_type: "email_capture",
+    slice_label: null,
+    primary: {
+        buttonText: "Join Today",
+        hash: null,
+        label: "Email",
+        optInContext: null,
+        optInCopy: [],
+        redirectUrl: null,
+        title: [
+            {
+                type: "heading1", 
+                text: "Ready to get started? Sign up for a free account.", 
+                spans: []
+            }
+        ]
+    },
+    items: []
+  } 
---
+++ pages/tenants/features/data/how-it-works.js
@@ -0,0 +1,93 @@
+export default {
+    slice_type: "how_it_works",
+    slice_label: null,
+    primary: {
+        background: "transparent",
+        flip: false,
+        hash: null,
+        title: [],
+    },
+    items: [
+        {
+            description: [
+                {
+                    type: "paragraph", 
+                    text: "Share your renter profile with as many landlords as you need. Your profile automatically updates with your residence history over time.", 
+                    spans: []
+                }
+            ],
+            embed: {},
+            image: {
+                dimensions: {
+                    height: 691,
+                    width: 1200
+                }, 
+                alt: null, 
+                copyright: null,
+                url: "/tenants/features/save_time.png"
+            },
+            title: [
+                {
+                    spans: [],
+                    text: "Save Time and Streamline The Process",
+                    type: "heading1"
+                }
+            ],
+            video: {link_type: "Media"}
+        },
+        {
+            description: [
+                {
+                    type: "paragraph", 
+                    text: "Access to an online rental portal allows you to easily sign, set up automatic rent payments and submit a maintenance request all in one place.", 
+                    spans: []
+                }
+            ],
+            embed: {},
+            image: {
+                dimensions: {
+                    height: 637,
+                    width: 993
+                }, 
+                alt: null, 
+                copyright: null,
+                url: "/tenants/features/simplify.png"
+            },
+            title: [
+                {
+                    spans: [],
+                    text: "Simplify Everyday Renting Tasks",
+                    type: "heading1"
+                }
+            ],
+            video: {link_type: "Media"}
+        },
+        {
+            description: [
+                {
+                    type: "paragraph", 
+                    text: "Build credit simply by paying your rent each month. Avail reports on-time rent payments to credit bureaus.", 
+                    spans: []
+                }
+            ],
+            embed: {},
+            image: {
+                dimensions: {
+                    height: 691,
+                    width: 1200
+                }, 
+                alt: null, 
+                copyright: null,
+                url: "/tenants/features/improve.png"
+            },
+            title: [
+                {
+                    spans: [],
+                    text: "Improve Your Credit",
+                    type: "heading1"
+                }
+            ],
+            video: {link_type: "Media"}
+        }
+    ]
+  } 
