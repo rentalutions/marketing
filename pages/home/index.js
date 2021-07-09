538ce953ce871af62a4f69ba6079c0900ee934ec
Natali Honda <natalihonda@gmail.com>
2021-07-09 15:05:25 -0300

    removing unused components

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,10 +2,6 @@ import React from "react"
 
 import StaticBasePage from "components/static/StaticBasePage"
 import SliceZone from "components/partials/SliceZone"
-import HomeHowItWorks from "./components/home-how-it-works"
-import HomeTestimonials from "./components/home-testimonials"
-import HomeShowcase from "./components/home-showcase"
-import HomeEmailCapture from "./components/home-email-capture"
 import data from "./data"
 
 const TenantFeatures = () => {
